0b0249ffdfcf6174908748d6e3e292a2044ed2c9
Reisyu <reisyu@gmail.com>
2017-06-04 16:13:56 +0900

    20170603　作成 20170604　1-2相励磁のイメージに変更

--- microStepTest.py
+++ microStepTest.py
@@ -1,7 +1,8 @@
 # coding: UTF-8
 # ステッピングモーターマイクロステップ制御用の数列を作成する
 # 20170603作成
-stepN = 4
+# 1-2相励磁のイメージに変更
+stepN = 2
 chanelN = 4
 
 stepList = []
@@ -9,15 +10,24 @@ stepList = []
 for i in range(chanelN):
     k0 = (i + 0) % chanelN
     k1 = (i + 1) % chanelN
+    k2 = (i + 2) % chanelN
     for j in range(stepN):
         tmp = []
         for k in range(chanelN):
             tmp.append(0)
         tmp[k0] = stepN - j
-        tmp[k1] = j
+        tmp[k1] = stepN
+        stepList.append(tmp)
+    for j in range(stepN):
+        tmp = []
+        for k in range(chanelN):
+            tmp.append(0)
+        tmp[k1] = stepN
+        tmp[k2] = j
         stepList.append(tmp)
 
-        #print(tmp)
+
+            #print(tmp)
 
 #print(stepList)
 
